b20cfd629d1dfec92470740c8bf43686e39a3f0d
fadope1 <fabian.peschke1@gmail.com>
2021-03-22 14:20:04 +0100

    fixed typo

--- speaker.py
+++ speaker.py
@@ -2,15 +2,15 @@
 class speaker:
     # TODO: Add possibility to play mp3 file
 
-    def __init(self, rate=100):
+    def __init__(self, rate=100):
         import pyttsx3
         self.engine = pyttsx3.init()
         self.engine.setProperty('voice', "german")
         self.engine.setProperty('rate', rate)
 
     def speak_file(self, file):
-        with open(file) as file:
-            content = file.read()
+        with open(file) as f:
+            content = f.read()
 
             self.speak(content)
 
